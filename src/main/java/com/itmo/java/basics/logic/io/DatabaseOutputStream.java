bc0471d7b801e9bdbd8123d7278ec49dbbcbe214
DnC275 <275.denis@gmail.com>
2021-03-27 13:40:57 +0300

    Final

--- src/main/java/com/itmo/java/basics/logic/io/DatabaseOutputStream.java
+++ src/main/java/com/itmo/java/basics/logic/io/DatabaseOutputStream.java
@@ -4,12 +4,30 @@ import com.itmo.java.basics.logic.WritableDatabaseRecord;
 
 import java.io.*;
 
+/**
+ * Записывает данные в БД
+ */
 public class DatabaseOutputStream extends DataOutputStream {
 
     public DatabaseOutputStream(OutputStream outputStream) {
         super(outputStream);
     }
 
+    /**
+     * Записывает в БД в следующем формате:
+     * - Размер ключа в байтах используя {@link WritableDatabaseRecord#getKeySize()}
+     * - Ключ
+     * - Размер записи в байтах {@link WritableDatabaseRecord#getValueSize()}
+     * - Запись
+     * Например при использовании UTF_8,
+     * "key" : "value"
+     * 3key5value
+     * Метод вернет 10
+     *
+     * @param databaseRecord запись
+     * @return размер записи
+     * @throws IOException если запись не удалась
+     */
     public long write(WritableDatabaseRecord databaseRecord) throws IOException {
         writeInt(databaseRecord.getKeySize());
         write(databaseRecord.getKey());
