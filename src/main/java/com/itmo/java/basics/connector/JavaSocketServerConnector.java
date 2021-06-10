725a06fc4f73ee4e32914f8cf88a7d413d76b970
DnC275 <275.denis@gmail.com>
2021-06-11 02:45:12 +0300

    Test

--- src/main/java/com/itmo/java/basics/connector/JavaSocketServerConnector.java
+++ src/main/java/com/itmo/java/basics/connector/JavaSocketServerConnector.java
@@ -145,7 +145,7 @@ public class JavaSocketServerConnector implements Closeable {
                     writer.write(result.serialize());
                 } catch (IOException | InterruptedException | ExecutionException e) {
 //                    System.out.println("Client task run error");
-//                    close();
+                    close();
                     throw new RuntimeException("Client task run error", e); //TODO
                 }
             }
